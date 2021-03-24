d8a13920804af1e17dce050943229dadd89edfd4
IaraSelva <71565887+IaraSelva@users.noreply.github.com>
2021-03-23 22:11:52 -0300

    criando a primeira service produto

--- organicos/src/main/java/com/ecommerce/organicos/controller/ProdutosController.java
+++ organicos/src/main/java/com/ecommerce/organicos/controller/ProdutosController.java
@@ -1,17 +1,18 @@
 package com.ecommerce.organicos.controller;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.organicos.model.Produtos;
-import com.ecommerce.organicos.repository.ProdutosRepository;
+import com.ecommerce.organicos.service.ProdutoService;
 
 @RestController
 @RequestMapping("/produtos")
@@ -19,15 +20,10 @@ import com.ecommerce.organicos.repository.ProdutosRepository;
 public class ProdutosController {
 	
 	@Autowired
-	private ProdutosRepository repository;
+	private ProdutoService service;
 	
-	@GetMapping
-	public ResponseEntity<List<Produtos>> getAll(){
-		return ResponseEntity.ok(repository.findAll());
-	}
-	
-	@GetMapping("/xepa")
-	public ResponseEntity<List<Produtos>> getByData(Date data){
-		return ResponseEntity.ok(repository.findByDataSafra(data));
+	@GetMapping("/organicos")
+	public ResponseEntity <List<Produtos>> listarOrganicos(@RequestParam(defaultValue = "") boolean organicos){
+		return new ResponseEntity <List<Produtos>> (service.listarOrganicos(organicos),HttpStatus.ACCEPTED);
 	}
 }
