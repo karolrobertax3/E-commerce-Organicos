94cdd7028fb7630982f9edd708b5d6d9b2a97ad4
unknown <helder825@hotmail.com>
2021-03-25 20:18:58 -0300

    Service e Crud de produto

--- organicos/src/main/java/com/ecommerce/organicos/service/ProdutoService.java
+++ organicos/src/main/java/com/ecommerce/organicos/service/ProdutoService.java
@@ -1,2 +1,54 @@
 package com.ecommerce.organicos.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.organicos.model.Produtos;
+import com.ecommerce.organicos.repository.ProdutosRepository;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	public ProdutosRepository repository;
+	
+	public List<Produtos> listarTodos(){
+		return repository.findAll();
+	}
+	
+	public Optional<Produtos> buscarPorId(Long id){
+		return repository.findById(id);
+	}
+	
+	public List<Produtos> listarOrganicos(boolean organico){
+		return repository.findAllByOrganico(organico);
+	}
+	
+	public Produtos postar(Produtos produtos) {
+		return repository.save(produtos);
+		
+	}
+	
+	public Optional<Produtos> alterar(Produtos produtos) {
+		Optional<Produtos> existente = repository.findById(produtos.getIdProduto());
+		if (existente.isEmpty()) {
+			return Optional.empty();
+		}
+		else {
+			existente.get().setCategoriaDoProduto(produtos.getCategoriaDoProduto());
+			existente.get().setDataSafra(produtos.getDataSafra());
+			existente.get().setDescricao(produtos.getDescricao());
+			existente.get().setNome(produtos.getNome());
+			existente.get().setOrganico(produtos.getOrganico());
+			existente.get().setPreco(produtos.getPreco());
+		}
+		   return Optional.ofNullable(repository.save(existente.get()));
+	}	
+		
+	public void deletar(Produtos produtos) {
+		repository.deleteById(produtos.getIdProduto());
+	}
+}
